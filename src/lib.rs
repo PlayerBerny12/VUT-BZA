c2d79e35a4de98f9f095f5e4c13bb4c34898acea
Jakub Reš <xresja@gmail.com>
2023-03-27 15:53:37 +0200

    XTEA working + small changes and inits

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Cryptographic library
 
-pub fn sub(left: usize, right: usize) -> usize {
-    left - right
-}
+#![allow(unused)]
+#![allow(non_snake_case)]
+#![warn(missing_docs)]
+
+mod manager_crypt;
 
 #[cfg(test)]
 mod tests {
@@ -12,25 +12,37 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        // let m = ManagerRNG {priority: 3};
-        let n = ManagerRNG::new(3);
-        println!("TU: {:#?}", n);
-        assert_eq!(result, 4);
+        let key = manager_crypt::xtea::Key([0,0,0,0]);
+        println!("{:#?}", key);
+        let mut data = [0,0];
+        manager_crypt::xtea::encrypt(64, &mut data, key);
+        println!("{} {}", data[0], data[1]);
+        // assert_eq!(key.0, [4, 4, 4, 4]);
+        assert!(data[0]==4237446418);
+        assert!(data[1]==1255206224);
+    }
+
+    #[test]
+    fn it_works_A() {
+
+        use std::{num::Wrapping};
+        let mut key = [Wrapping(0u32), Wrapping(0u32), Wrapping(0u32), Wrapping(0u32)];
+        let mut v0 = Wrapping(0u32);
+		let mut v1 = Wrapping(0u32);
+		let mut sum = Wrapping(0u32);
+
+		for _ in 0..64 as u32 {
+			v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[(sum.0 & 3) as usize]);
+			sum += Wrapping(0x9E3779B9);
+			v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[((sum.0 >> 11) & 3) as usize]);
+		}
+
+		println!("{} {}",  v0.0, v1.0);
+        assert!(1==1);
     }
 }
 
-
-#[derive(Debug)]
-struct ManagerRNG {priority: i32}
-
-impl ManagerRNG {
-    pub fn new(prio: i32) -> ManagerRNG {
-        ManagerRNG {priority: prio}
-    }
-}
-
-
-trait NextNumber {
-
+// PLACEHOLDER for get_next_rand funciton
+fn next() -> u32 {
+    1
 }
---
+++ src/manager_crypt/xtea/mod.rs
@@ -0,0 +1,52 @@
+//! eXtended Tiny Encryption Algorithm module
+
+use std::{num::Wrapping};
+
+#[derive(Debug)]
+pub struct Key(pub [u32; 4]);
+
+
+// Encrypts data using num_rounds of rounds of XTEA algorithm using key
+pub fn encrypt(num_rounds: u32, data: &mut [u32; 2], key: Key) {
+    let mut v0: Wrapping<u32> = Wrapping(data[0]);
+    let mut v1: Wrapping<u32> = Wrapping(data[1]);
+    let mut sum: Wrapping<u32> = Wrapping(0);
+    let mut delta: Wrapping<u32> = Wrapping(0x9E3779B9);
+
+    for _ in 0 .. num_rounds {
+        v0 += ((((v1 << 4) ^ (v1 >> 5)) + v1) ^ Wrapping(sum.0 + key.0[(sum.0 & 3) as usize]));
+        sum += delta;
+        v1 += ((((v0 << 4) ^ (v0 >> 5)) + v0) ^ Wrapping(sum.0 + key.0[((sum.0>>11) & 3) as usize]));
+    }
+    
+    data[0] = v0.0;
+    data[1] = v1.0;
+}
+
+// Decrypts data using num_rounds of rounds of XTEA algorithm using key
+pub fn decrypt(num_rounds: u32, data: &mut [u32; 2], key: Key) {
+    let mut v0: Wrapping<u32> = Wrapping(data[0]);
+    let mut v1: Wrapping<u32> = Wrapping(data[1]);
+    let mut delta: Wrapping<u32> = Wrapping(0x9E3779B9);
+    let mut sum: Wrapping<u32> = Wrapping(delta.0 * num_rounds);
+
+    for _ in 0 .. num_rounds {
+        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ Wrapping(sum.0 + key.0[((sum.0>>11) & 3) as usize]);
+        sum -= delta;
+        v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ Wrapping(sum.0 + key.0[(sum.0 & 3) as usize]);
+    }
+
+    data[0] = v0.0;
+    data[1] = v1.0;
+}
+
+// Generates keypair for XTEA encryption/decryption
+pub fn gen_key(next_rn: &dyn Fn() -> u32) -> Key {
+    let mut k = Key([0, 0, 0, 0]);
+    for i in k.0.iter_mut() {
+        *i = next_rn();
+    }
+
+    assert!(k.0 != [0, 0, 0, 0]);
+    k
+}

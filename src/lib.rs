71c874d3a8a17411d1acd6b0bbafa5cef7304a15
Jakub Reš <xresja@gmail.com>
2023-03-27 15:59:03 +0200

    Cleaning of test code

--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,6 @@ mod tests {
         assert!(data[0]==4237446418);
         assert!(data[1]==1255206224);
     }
-
-    #[test]
-    fn it_works_A() {
-
-        use std::{num::Wrapping};
-        let mut key = [Wrapping(0u32), Wrapping(0u32), Wrapping(0u32), Wrapping(0u32)];
-        let mut v0 = Wrapping(0u32);
-		let mut v1 = Wrapping(0u32);
-		let mut sum = Wrapping(0u32);
-
-		for _ in 0..64 as u32 {
-			v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[(sum.0 & 3) as usize]);
-			sum += Wrapping(0x9E3779B9);
-			v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[((sum.0 >> 11) & 3) as usize]);
-		}
-
-		println!("{} {}",  v0.0, v1.0);
-        assert!(1==1);
-    }
 }
 
 // PLACEHOLDER for get_next_rand funciton

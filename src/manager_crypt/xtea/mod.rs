218f41f27da87149b87ce2ba42d843be0908aabe
PlayerBerny12 <ber.honza12@seznam.cz>
2023-03-31 17:34:48 +0000

    Github action unit tests, devcontainer, move unit tests to lower modules

--- src/manager_crypt/xtea/mod.rs
+++ src/manager_crypt/xtea/mod.rs
@@ -1,12 +1,11 @@
 //! eXtended Tiny Encryption Algorithm module
 
-use std::{num::Wrapping};
+use std::num::Wrapping;
 
 pub struct XTEAConfig;
 
 #[derive(Debug)]
 pub struct Key(pub [u32; 4]);
-
 
 // Encrypts data using num_rounds of rounds of XTEA algorithm using key
 pub fn encrypt(num_rounds: u32, data: &mut [u32; 2], key: Key) {
@@ -15,12 +14,13 @@ pub fn encrypt(num_rounds: u32, data: &mut [u32; 2], key: Key) {
     let mut sum: Wrapping<u32> = Wrapping(0);
     let mut delta: Wrapping<u32> = Wrapping(0x9E3779B9);
 
-    for _ in 0 .. num_rounds {
+    for _ in 0..num_rounds {
         v0 += ((((v1 << 4) ^ (v1 >> 5)) + v1) ^ Wrapping(sum.0 + key.0[(sum.0 & 3) as usize]));
         sum += delta;
-        v1 += ((((v0 << 4) ^ (v0 >> 5)) + v0) ^ Wrapping(sum.0 + key.0[((sum.0>>11) & 3) as usize]));
+        v1 += ((((v0 << 4) ^ (v0 >> 5)) + v0)
+            ^ Wrapping(sum.0 + key.0[((sum.0 >> 11) & 3) as usize]));
     }
-    
+
     data[0] = v0.0;
     data[1] = v1.0;
 }
@@ -32,8 +32,9 @@ pub fn decrypt(num_rounds: u32, data: &mut [u32; 2], key: Key) {
     let mut delta: Wrapping<u32> = Wrapping(0x9E3779B9);
     let mut sum: Wrapping<u32> = Wrapping(delta.0 * num_rounds);
 
-    for _ in 0 .. num_rounds {
-        v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ Wrapping(sum.0 + key.0[((sum.0>>11) & 3) as usize]);
+    for _ in 0..num_rounds {
+        v1 -=
+            (((v0 << 4) ^ (v0 >> 5)) + v0) ^ Wrapping(sum.0 + key.0[((sum.0 >> 11) & 3) as usize]);
         sum -= delta;
         v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ Wrapping(sum.0 + key.0[(sum.0 & 3) as usize]);
     }
@@ -51,4 +52,21 @@ pub fn gen_key(next_rn: &dyn Fn() -> u32) -> Key {
 
     assert!(k.0 != [0, 0, 0, 0]);
     k
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_encrypt() {
+        let key = Key([0, 0, 0, 0]);
+        println!("{:#?}", key);
+        let mut data = [0, 0];
+        encrypt(64, &mut data, key);
+        println!("{} {}", data[0], data[1]);
+        // assert_eq!(key.0, [4, 4, 4, 4]);
+        assert!(data[0] == 4237446418);
+        assert!(data[1] == 1255206224);
+    }
 }
